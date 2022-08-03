39454575d4128074d74d579b67684a97d82314f6
harveyshao <harveyshao@tencent.com>
2022-08-03 08:15:48 +0800

    opt base

--- src/grafana/grafana.go
+++ src/grafana/grafana.go
@@ -6,6 +6,7 @@ import (
 
 	"shylinux.com/x/ice"
 	"shylinux.com/x/icebergs/base/mdb"
+	"shylinux.com/x/icebergs/base/web"
 	"shylinux.com/x/icebergs/core/chat/oauth"
 	kit "shylinux.com/x/toolkits"
 )
@@ -19,8 +20,8 @@ type grafana struct {
 func (s grafana) Start(m *ice.Message, arg ...string) {
 	s.Code.Start(m, "", "bin/grafana-server", func(p string) {
 		port := path.Base(p)
-		web := m.Option(ice.MSG_USERWEB)
-		hostname := m.OptionUserWeb().Hostname()
+		url := m.Option(ice.MSG_USERWEB)
+		hostname := web.OptionUserWeb(m.Message).Hostname()
 		client_id := m.Cmdx(oauth.Prefix(oauth.AUTHORIZE), mdb.CREATE, oauth.REDIRECT_URI, kit.Format("http://%s:%s/login/generic_oauth", hostname, port))
 		value := kit.KeyValue(nil, "", kit.Dict(
 			"server", kit.Dict("domain", hostname, "http_addr", hostname, "http_port", port),
@@ -33,9 +34,9 @@ func (s grafana) Start(m *ice.Message, arg ...string) {
 				// "disable_signout_menu", "true",
 				"generic_oauth", kit.Dict(
 					"enabled", "true", "client_id", client_id, "scopes", "openid profile email",
-					"auth_url", kit.MergeURL2(web, "/chat/oauth/authorize"),
-					"token_url", kit.MergeURL2(web, "/chat/oauth/token"),
-					"api_url", kit.MergeURL2(web, "/chat/oauth/userinfo"),
+					"auth_url", kit.MergeURL2(url, "/chat/oauth/authorize"),
+					"token_url", kit.MergeURL2(url, "/chat/oauth/token"),
+					"api_url", kit.MergeURL2(url, "/chat/oauth/userinfo"),
 				),
 			),
 		))
