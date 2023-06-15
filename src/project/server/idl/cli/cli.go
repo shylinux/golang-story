810f896f9b965387cbc219010abbaacf6665d630
shylinux <shylinux@163.com>
2023-06-15 17:25:45 +0800

    opt some

--- src/project/server/idl/cli/cli.go
+++ src/project/server/idl/cli/cli.go
@@ -8,9 +8,9 @@ import (
 func Init(container *container.Container) {
 	container.Provide(NewMainServiceCmds)
 
-	container.Provide(NewAuthServiceCmds)
-
 	container.Provide(NewUserServiceCmds)
+
+	container.Provide(NewAuthServiceCmds)
 
 }
 
@@ -18,6 +18,6 @@ type MainServiceCmds struct {
 	*cmds.Cmds
 }
 
-func NewMainServiceCmds(cmds *cmds.Cmds, _ *AuthServiceCmds, _ *UserServiceCmds) *MainServiceCmds {
+func NewMainServiceCmds(cmds *cmds.Cmds, _ *UserServiceCmds, _ *AuthServiceCmds) *MainServiceCmds {
 	return &MainServiceCmds{cmds}
 }
